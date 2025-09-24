3f970754d8fd44504ce0b81d83c6289d5ddd6f0d
wuchuheng <root@wuchuheng.com>
2025-09-24 17:03:57 +0800

    refactory: set up libary based on vite.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
+import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
@@ -19,6 +15,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
---
+++ vite.config.ts
@@ -0,0 +1,35 @@
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+  build: {
+    // Automatically clean the output directory before build
+    emptyOutDir: true,
+    lib: {
+      // Entry point of your library
+      entry: "src/main.ts",
+      // Generate files with correct naming
+      fileName: (format) => `index.${format}.js`,
+      // Only build ES modules since you only support browsers
+      formats: ["es"],
+    },
+    rollupOptions: {
+      // Externalize dependencies that shouldn't be bundled
+      // Add any peer dependencies here
+      external: [],
+      output: {
+        // Global variables for any external dependencies
+        globals: {},
+      },
+    },
+    // Generate sourcemaps for better debugging
+    sourcemap: true,
+    // Target modern browsers
+    target: "esnext",
+  },
+});
